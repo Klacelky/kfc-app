ceb77017f0f4da1c6dd2a5f008e409ca1b983d80
Juraj Marcin <juraj@jurajmarcin.com>
2023-11-11 02:16:25 +0100

    add links

---
+++ utils/links.ts
@@ -0,0 +1,3 @@
+export const RESERVATION_SK = 'https://cal.jirik.dev/kfc/skupina?layout=week_view';
+export const RESERVATION_EN = 'https://cal.jirik.dev/kfc/group?layout=week_view';
+export const FB_EVENT = 'https://www.facebook.com/events/227964476781108';
