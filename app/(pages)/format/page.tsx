839958e337551270bb7c975eea2336d0284d02ba
Juraj Marcin <juraj@jurajmarcin.com>
2023-11-12 19:30:34 +0100

    Add group scoring

--- app/(pages)/format/page.tsx
+++ app/(pages)/format/page.tsx
@@ -175,6 +175,44 @@ export default function FormatPage() {
                         }
                     />
                 </p>
+                <section>
+                    <h3>
+                        <T sk="Bodovanie" en="Scoring" />
+                    </h3>
+                    <p>
+                        <T
+                            sk="Za každý vyhraný zápas (BO3) dostane tím 1 bod."
+                            en="For each match won (BO3), the team receives 1 point."
+                        />
+                    </p>
+                    <p>
+                        <T
+                            sk={
+                                <>
+                                    Poradie v skupine sa určí primárne podľa počtu bodov (vyhratých zápasov). V prípade
+                                    rovnosti bodov rozhoduje výsledok vzájomného zápasu v skupine. V prípade, že takto
+                                    vznikne cyklus (tímy A, B, C majú zhodný počet bodov a A vyhralo nad B, B vyhralo
+                                    nad C a C vyhralo nad A), rozhoduje rozdiel medzi počtom vyhraných a počtom
+                                    prehratých hier vo vzájomných zápasoch týchto troch tímov. Ak je aj tento rozdiel
+                                    rovnaký, tak rozhoduje rozdiel medzi počtom strelených a inkasovaných gólov. Ak je
+                                    aj tento rozdiel rovnaký, tak poradie bude určené náhodným žrebom.
+                                </>
+                            }
+                            en={
+                                <>
+                                    The group standings will be determined primarily by the number of points (matches
+                                    won). In case of a tie points are equal, the result of the mutual match in the group
+                                    is decisive. In the event that this creates a cycle (teams A, B, C have the same
+                                    number of points and A wins over B, B wins over C and C wins over A), the
+                                    tie-breaker shall be the difference between the number of games won and the number
+                                    of games lost between the three teams. If this difference is also the same, the
+                                    difference between the number of goals scored and the number of goals scored. If
+                                    this difference is also equal, the order will be determined by a random draw.
+                                </>
+                            }
+                        />
+                    </p>
+                </section>
             </section>
             <section>
                 <h2>
@@ -210,6 +248,121 @@ export default function FormatPage() {
                             <>
                                 There are 30 minutes for each match. As foosball is not played on time, the schedule is
                                 approximate and there may be slight shifts in match times.
+                            </>
+                        }
+                    />
+                </p>
+            </section>
+            <section>
+                <h2>
+                    <T sk="Cena Bc. Petra Burdu" en="Bc. Peter Burda award" />
+                </h2>
+                <p>
+                    <T
+                        sk={
+                            <>
+                                Na vaše želanie a motivovanie nie tak skúsených hráčov sme sa inšpirovali KFO a rovnako
+                                zavedieme cenu Bc. Petra Burdu. Cenu získa tím, ktorý skončí v celom turnaji s najmenej
+                                bodmi a najhorším skóre. Cenu už tradične odovzdá sám Bc. Peter Burda prostredníctvom
+                                online spojenia. Cenou bude vopred nešpecifikovaný alkoholický výrobok.
+                            </>
+                        }
+                        en={
+                            <>
+                                At your request and to motivate the not so experienced players, we have taken
+                                inspiration from KFO and will be introducing the Bc. Peter Burda award. The award will
+                                be awarded to the team that finishes with the fewest points and the worst score in the
+                                entire tournament. The prize will traditionally be presented by Bc. Peter Burda. The
+                                award will be an unspecified alcoholic product.
+                            </>
+                        }
+                    />
+                </p>
+                <p>
+                    <T
+                        sk={
+                            <>
+                                Poznámka od organizátorov: Pokiaľ budú 2 tímy (najhoršie) so zhodným skóre, rozhodovať
+                                bude vzájomný zápas medzi týmito tímami, má to však háčik, cenu získa výherný z týchto
+                                dvoch tímov, porazeným zostane iba hanba, že sú horší, ako najhorší. Predpokladáme, že
+                                takáto situácia nenastane, ale pokiaľ áno, nech je z toho aspoň trochu sranda.
+                            </>
+                        }
+                        en={
+                            <>
+                                Note from the organizers: if there are 2 teams (worst) with identical scores, the match
+                                between these teams will be decided. But there is a catch, the prize will go to the
+                                winning of these two teams, the losers will be left with only the shame of being worse
+                                than the worst. We don{"'"}t expect that situation to arise, but if it does, let{"'"}s
+                                make it at least a little fun.
+                            </>
+                        }
+                    />
+                </p>
+            </section>
+            <section>
+                <h2>
+                    <T sk="Cena pre naj strelca a Zlatý Jožo" en="The MVP award and “Zlatý Jožo” (The Golden Joe)" />
+                </h2>
+                <p>
+                    <T
+                        sk={
+                            <>
+                                Nebudú chýbať ani individuálne ocenenia. V pláne máme novinku, a to je cena pre hráča,
+                                ktorý počas play-off strelí najrýchlejší gól (nie v sekundách ale m/s!). Ako to spravíme
+                                sa nechajte prekvapiť. Pre prípad, že sa nám to nepodarí tak, ako plánujeme, cenu naj
+                                strelca získa najproduktívnejší hráč.
+                            </>
+                        }
+                        en={
+                            <>
+                                There will be no shortage of individual awards either. We are planning a new one and
+                                that is an award for the player who scores the fastest goal during the play-off (not in
+                                seconds but m/s!). How will we do it? It is a surprise. In case it doesn{"'"}t go as we
+                                plan, the most productive player will get the MVP award.
+                            </>
+                        }
+                    />
+                </p>
+            </section>
+            <section>
+                <h2>
+                    <T sk="Stream" en="Stream" />
+                </h2>
+                <p>
+                    <T
+                        sk={
+                            <>
+                                Aby sme hráčom dopriali priestor na hranie a keďže okolo stola nie je veľa miesta pre
+                                divákov, zápasy play-off budú live streamované na telke v spoločenskej miestnosti hneď
+                                veďla kalčeta a online, aby ste pri sledovaní neprišli o atmosféru a emócie.
+                            </>
+                        }
+                        en={
+                            <>
+                                In order to give the players room to play and since there is not much space around the
+                                table for spectators, the playoff games will be live-streamed on the TV in the TV room
+                                right next to the table and online, so that you don{"'"}t miss the atmosphere and
+                                emotions while watching.
+                            </>
+                        }
+                    />
+                </p>
+                <p>
+                    <T sk="Link na stream neskôr nájdeš tu." en="You can find the link to the stream later here." />
+                </p>
+                <p>
+                    <T
+                        sk={
+                            <>
+                                Skupinové zápasy streamované nebudú. Ak sa chcete prísť pozrieť na nejaký skupinový
+                                zápas dohodnite sa s hráčmi.
+                            </>
+                        }
+                        en={
+                            <>
+                                Group matches will not be streamed. If you want to come and watch a group match, please
+                                ask players if they don’t mind.
                             </>
                         }
                     />
--- app/(pages)/play-off/page.tsx
+++ app/(pages)/play-off/page.tsx
@@ -12,7 +12,7 @@ interface ScheduleData {
 
 function Schedule({ matches }: ScheduleData) {
     return (
-        <div className="grid gap-x-4 gap-y-2 grid-cols-1 sm:grid-cols-11 max-w-2xl">
+        <div className="grid gap-x-4 gap-y-2 grid-cols-1 sm:grid-cols-12 max-w-2xl">
             {matches.map(({ name, time, home, visiting }) => (
                 <Fragment key={name?.toString()}>
                     <div className="sm:col-span-3 flex flex-row gap-x-1 sm:justify-between">
@@ -36,14 +36,14 @@ export default function PlayOffPage() {
             </p>
             <section>
                 <h2>
-                    <T sk="Baráž" en="Barage" />
+                    <T sk="Baráž" en="Promotion and relegation" />
                 </h2>
                 <Schedule
                     matches={[
-                        { name: 'Br1', time: new Date(2023, 11, 18, 13, 15, 0), home: 'A2', visiting: 'D3' },
-                        { name: 'Br2', time: new Date(2023, 11, 18, 13, 45, 0), home: 'B2', visiting: 'C3' },
-                        { name: 'Br3', time: new Date(2023, 11, 18, 14, 15, 0), home: 'C2', visiting: 'B3' },
-                        { name: 'Br4', time: new Date(2023, 11, 18, 14, 45, 0), home: 'D2', visiting: 'A3' },
+                        { name: 'Br I', time: new Date(2023, 11, 18, 13, 15, 0), home: 'A2', visiting: 'D3' },
+                        { name: 'Br II', time: new Date(2023, 11, 18, 13, 45, 0), home: 'B2', visiting: 'C3' },
+                        { name: 'Br III', time: new Date(2023, 11, 18, 14, 15, 0), home: 'C2', visiting: 'B3' },
+                        { name: 'Br IV', time: new Date(2023, 11, 18, 14, 45, 0), home: 'D2', visiting: 'A3' },
                     ]}
                 />
                 <p>
@@ -56,10 +56,10 @@ export default function PlayOffPage() {
                 </h2>
                 <Schedule
                     matches={[
-                        { name: 'Qf1', time: new Date(2023, 11, 18, 15, 30, 0), home: 'A1', visiting: 'Br2' },
-                        { name: 'Qf2', time: new Date(2023, 11, 18, 16, 0, 0), home: 'B1', visiting: 'Br1' },
-                        { name: 'Qf3', time: new Date(2023, 11, 18, 16, 30, 0), home: 'C1', visiting: 'Br4' },
-                        { name: 'Qf4', time: new Date(2023, 11, 18, 17, 0, 0), home: 'D1', visiting: 'Br3' },
+                        { name: 'Qf I', time: new Date(2023, 11, 18, 15, 30, 0), home: 'A1', visiting: 'Br II' },
+                        { name: 'Qf II', time: new Date(2023, 11, 18, 16, 0, 0), home: 'B1', visiting: 'Br I' },
+                        { name: 'Qf III', time: new Date(2023, 11, 18, 16, 30, 0), home: 'C1', visiting: 'Br IV' },
+                        { name: 'Qf IV', time: new Date(2023, 11, 18, 17, 0, 0), home: 'D1', visiting: 'Br III' },
                     ]}
                 />
                 <p>
@@ -72,8 +72,8 @@ export default function PlayOffPage() {
                 </h2>
                 <Schedule
                     matches={[
-                        { name: 'Sf1', time: new Date(2023, 11, 18, 17, 45, 0), home: 'Qf1', visiting: 'Qf4' },
-                        { name: 'Sf2', time: new Date(2023, 11, 18, 13, 45, 0), home: 'Qf2', visiting: 'Qf3' },
+                        { name: 'Sf I', time: new Date(2023, 11, 18, 17, 45, 0), home: 'Qf I', visiting: 'Qf IV' },
+                        { name: 'Sf II', time: new Date(2023, 11, 18, 13, 45, 0), home: 'Qf II', visiting: 'Qf III' },
                     ]}
                 />
                 <p>
@@ -89,14 +89,14 @@ export default function PlayOffPage() {
                         {
                             name: <T sk="3. miesto" en="3rd place" />,
                             time: new Date(2023, 11, 18, 19, 0, 0),
-                            home: 'Sf1L',
-                            visiting: 'Sf2L',
+                            home: 'Sf I L',
+                            visiting: 'Sf II L',
                         },
                         {
                             name: <T sk="Finále" en="Finals" />,
                             time: new Date(2023, 11, 18, 19, 30, 0),
-                            home: 'Sf1W',
-                            visiting: 'Sf2W',
+                            home: 'Sf I W',
+                            visiting: 'Sf II W',
                         },
                     ]}
                 />
