f83d177228773491daa0bf1435c9e37310453a07
Juraj Marcin <juraj@jurajmarcin.com>
2023-11-18 16:53:38 +0100

    scores after qf

--- app/(pages)/play-off/page.tsx
+++ app/(pages)/play-off/page.tsx
@@ -1,4 +1,5 @@
 import T from '@/utils/i18n/t';
+import classNames from 'classnames';
 import { Fragment, ReactNode } from 'react';
 
 interface ScheduleData {
@@ -7,20 +8,37 @@ interface ScheduleData {
         time: Date;
         home: string;
         visiting: string;
+        results?: [number, number][];
     }[];
 }
 
 function Schedule({ matches }: ScheduleData) {
     return (
         <div className="grid gap-x-4 gap-y-2 grid-cols-1 sm:grid-cols-12 max-w-5xl">
-            {matches.map(({ name, time, home, visiting }) => (
+            {matches.map(({ name, time, home, visiting, results }) => (
                 <Fragment key={name?.toString()}>
                     <div className="sm:col-span-6 flex flex-row gap-x-1 sm:justify-between">
                         <div>{name}</div>
                         <div>{time.toLocaleTimeString()}</div>
                     </div>
-                    <div className="sm:col-span-3">{home}</div>
-                    <div className="sm:col-span-3">{visiting}</div>
+                    <div
+                        className={classNames('col-span-1 sm:text-right', {
+                            'font-bold': (results?.filter(([homeScore, _]) => homeScore == 10).length || 0) >= 3,
+                        })}
+                    >
+                        {home}
+                    </div>
+                    <div className="sm:col-span-4 sm:text-center">
+                        {results?.map(([homeScore, visitingScore]) => `${homeScore}:${visitingScore}`).join(', ')}
+                    </div>
+                    <div
+                        className={classNames('sm:col-span-1', {
+                            'font-bold':
+                                (results?.filter(([_, visitingScore]) => visitingScore == 10).length || 0) >= 3,
+                        })}
+                    >
+                        {visiting}
+                    </div>
                 </Fragment>
             ))}
         </div>
@@ -40,10 +58,50 @@ export default function PlayOffPage() {
                 </h2>
                 <Schedule
                     matches={[
-                        { name: 'Br I', time: new Date(2023, 11, 18, 12, 0, 0), home: 'NPV', visiting: 'OCO' },
-                        { name: 'Br II', time: new Date(2023, 11, 18, 12, 30, 0), home: 'KKT', visiting: 'DIF' },
-                        { name: 'Br III', time: new Date(2023, 11, 18, 13, 0, 0), home: 'MID', visiting: 'ZMK' },
-                        { name: 'Br IV', time: new Date(2023, 11, 18, 13, 30, 0), home: 'MSB', visiting: 'TAP' },
+                        {
+                            name: 'Br I',
+                            time: new Date(2023, 11, 18, 12, 0, 0),
+                            home: 'NPV',
+                            visiting: 'OCO',
+                            results: [
+                                [10, 0],
+                                [10, 0],
+                                [10, 9],
+                            ],
+                        },
+                        {
+                            name: 'Br II',
+                            time: new Date(2023, 11, 18, 12, 30, 0),
+                            home: 'KKT',
+                            visiting: 'DIF',
+                            results: [
+                                [10, 3],
+                                [10, 5],
+                                [10, 3],
+                            ],
+                        },
+                        {
+                            name: 'Br III',
+                            time: new Date(2023, 11, 18, 13, 0, 0),
+                            home: 'MID',
+                            visiting: 'ZMK',
+                            results: [
+                                [10, 6],
+                                [10, 6],
+                                [10, 3],
+                            ],
+                        },
+                        {
+                            name: 'Br IV',
+                            time: new Date(2023, 11, 18, 13, 30, 0),
+                            home: 'MSB',
+                            visiting: 'TAP',
+                            results: [
+                                [5, 10],
+                                [8, 10],
+                                [1, 10],
+                            ],
+                        },
                     ]}
                 />
                 <p>
@@ -56,10 +114,51 @@ export default function PlayOffPage() {
                 </h2>
                 <Schedule
                     matches={[
-                        { name: 'Qf I', time: new Date(2023, 11, 18, 14, 30, 0), home: 'BBH', visiting: 'Br II' },
-                        { name: 'Qf II', time: new Date(2023, 11, 18, 15, 0, 0), home: 'ORG', visiting: 'Br I' },
-                        { name: 'Qf III', time: new Date(2023, 11, 18, 15, 30, 0), home: 'KYM', visiting: 'Br IV' },
-                        { name: 'Qf IV', time: new Date(2023, 11, 18, 16, 0, 0), home: 'PAC', visiting: 'Br III' },
+                        {
+                            name: 'Qf I',
+                            time: new Date(2023, 11, 18, 14, 30, 0),
+                            home: 'BBH',
+                            visiting: 'KKT',
+                            results: [
+                                [10, 2],
+                                [10, 2],
+                                [10, 7],
+                            ],
+                        },
+                        {
+                            name: 'Qf II',
+                            time: new Date(2023, 11, 18, 15, 0, 0),
+                            home: 'ORG',
+                            visiting: 'NPV',
+                            results: [
+                                [10, 3],
+                                [9, 10],
+                                [2, 10],
+                                [9, 10],
+                            ],
+                        },
+                        {
+                            name: 'Qf III',
+                            time: new Date(2023, 11, 18, 15, 30, 0),
+                            home: 'KYM',
+                            visiting: 'TAP',
+                            results: [
+                                [10, 1],
+                                [10, 5],
+                                [10, 2],
+                            ],
+                        },
+                        {
+                            name: 'Qf IV',
+                            time: new Date(2023, 11, 18, 16, 0, 0),
+                            home: 'PAC',
+                            visiting: 'MID',
+                            results: [
+                                [10, 5],
+                                [10, 5],
+                                [10, 9],
+                            ],
+                        },
                     ]}
                 />
                 <p>
