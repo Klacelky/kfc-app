2dc97f99b6479c3f993fa7be449ac3183f83de6d
Juraj Marcin <juraj@jurajmarcin.com>
2024-03-14 17:23:16 +0100

    prisma: Add Player schema

---
+++ prisma/migrations/20240314162213_player/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Player" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL DEFAULT '',
+    "photo" TEXT,
+
+    CONSTRAINT "Player_pkey" PRIMARY KEY ("id")
+);
