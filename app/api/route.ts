0e6cded7c2c51fae6693832c68a3c7ef2aa28739
Juraj Marcin <juraj@jurajmarcin.com>
2024-04-08 01:29:11 +0200

    api: add base status route

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,1 +1,13 @@
+import { AuthNextRequest, RouteContext, auth, handle } from '@/utils/server/api';
+
 export interface RouteParams {}
+
+export const GET = handle(
+    auth({ optional: true }, async (request: AuthNextRequest, context: RouteContext<RouteParams>) => {
+        return Response.json({
+            status: 'OK, I guess?',
+            auth: request.auth,
+            context,
+        });
+    }),
+);
