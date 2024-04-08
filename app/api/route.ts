f781ff0be566a1e822cfe18e6193a22967602649
Juraj Marcin <juraj@jurajmarcin.com>
2024-04-08 02:07:57 +0200

    api: add db check to status route

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,11 +1,13 @@
 import { AuthNextRequest, RouteContext, auth, handle } from '@/utils/server/api';
+import prisma from '@/utils/server/db';
 
 export interface RouteParams {}
 
 export const GET = handle(
     auth({ optional: true }, async (request: AuthNextRequest, context: RouteContext<RouteParams>) => {
+        const adminCount = await prisma.adminUser.count();
         return Response.json({
-            status: 'OK, I guess?',
+            status: adminCount ? 'OK, I guess?' : 'OK, but alone...',
             auth: request.auth,
             context,
         });
