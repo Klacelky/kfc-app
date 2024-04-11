ee127b27775512440d3d39cf4efda7e004aa9b96
Juraj Marcin <juraj@jurajmarcin.com>
2024-04-11 02:38:05 +0200

    admin: Add Alert component

---
+++ components/admin/Alert.tsx
@@ -0,0 +1,22 @@
+import { CheckCircleIcon, ExclamationCircleIcon } from '@heroicons/react/16/solid';
+import classNames from 'classnames';
+import { ReactNode } from 'react';
+
+export interface AlertProps {
+    type?: 'error' | 'success';
+    children: ReactNode;
+}
+
+export default function Alert({ type, children }: AlertProps) {
+    return (
+        <div
+            className={classNames('p-4 flex flex-row gap-2 items-center text-white font-bold', {
+                'bg-red-600': !type || type === 'error',
+                'bg-green-600': type === 'success',
+            })}
+        >
+            {type === 'success' ? <CheckCircleIcon className="w-5" /> : <ExclamationCircleIcon className="w-5" />}
+            {children}
+        </div>
+    );
+}
