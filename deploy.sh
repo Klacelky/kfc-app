ee4f7f5c15a9e7aa3ded07c477b3a7eb61c2fb58
Juraj Marcin <juraj@jurajmarcin.com>
2024-10-30 22:50:01 +0100

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,17 @@
 set -e
 set -x
 
+git stash
+
+if [ "$1" = "force" ]; then
+    git fetch --all
+    git reset --hard origin/main
+else
+    git pull --rebase
+fi
+
+git stash pop
+
 podman compose build --pull
 
 podman push registry.jirik.dev/kfc/app:latest
