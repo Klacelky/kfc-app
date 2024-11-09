88d57132bf58ca889f70ec4e1bf5c9c7553f2549
Juraj Marcin <juraj@jurajmarcin.com>
2024-11-09 12:17:48 +0100

    Add special tag in deploy scripy

--- deploy.sh
+++ deploy.sh
@@ -16,8 +16,15 @@ git stash pop
 
 podman compose build --pull
 
+TAG=`date --utc +"%Y%m%d%H%M%S"`
+
+podman tag registry.jirik.dev/kfc/app:latest registry.jirik.dev/kfc/app:$TAG
+podman tag registry.jirik.dev/kfc/prisma:latest registry.jirik.dev/kfc/prisma:$TAG
+
 podman push registry.jirik.dev/kfc/app:latest
+podman push registry.jirik.dev/kfc/app:$TAG
 podman push registry.jirik.dev/kfc/prisma:latest
+podman push registry.jirik.dev/kfc/prisma:$TAG
 
 sudo podman pull registry.jirik.dev/kfc/app:latest
 sudo podman pull registry.jirik.dev/kfc/prisma:latest
