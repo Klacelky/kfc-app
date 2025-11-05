b8686c526014e7e6312fe28ea8ba0e21d019d2c3
Juraj Marcin <juraj@jurajmarcin.com>
2025-11-06 00:38:41 +0100

    Update home banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 
 import Header from '@/components/Header';
-import bannerImg from '@/public/banner_web_autumn2024.svg';
+import bannerImg from '@/public/banner_web.svg';
 
 export default function Home() {
     return (
